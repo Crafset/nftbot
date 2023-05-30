f615283f399f746832169a3979bca7e0af97675d
Crafset <82468619+Crafset@users.noreply.github.com>
2023-05-30 13:57:32 +0200

    Update nftbot.py

--- nftbot.py
+++ nftbot.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         print("Balises h3 et liens dans la classe 'inside-article' :")
         for h3, link in h3_and_links:
             if h3:
-                print("Balise h3 :", h3)
+                print("Name :", h3, "Lien :",link)
             if link:
                 print("Lien :", link)
     else:

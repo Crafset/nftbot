c78762f33b4edfe4629ae6bda113fcff0ba80178
Crafset <82468619+Crafset@users.noreply.github.com>
2023-06-03 22:26:40 +0200

    Update nftbot.py

--- nftbot.py
+++ nftbot.py
@@ -1,53 +1,27 @@
+import discord
 import requests
-from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
-from colorama import Fore, Style
+import asyncio
 
-def get_h3_and_links(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
+source_guild_id = "1046516981907591279"  # Remplacez "ID_DU_SERVEUR_SOURCE" par l'ID réel du serveur source
+category_id = "1091438158404649091"  # Remplacez "ID_DE_LA_CATEGORIE" par l'ID réel de la catégorie
+webhook_url = "URL_DU_WEBHOOK"  # Remplacez "URL_DU_WEBHOOK" par l'URL réelle du webhook de votre serveur Discord
 
-    inside_article_tags = soup.find_all(class_="inside-article")
-    h3_and_links = []
+class MyClient(discord.Client):
+    async def on_guild_channel_create(self, channel):
+        if isinstance(channel, discord.TextChannel) and channel.guild.id == source_guild_id and channel.category_id == category_id:
+            await asyncio.sleep(300)  # Attendre 5 minutes (300 secondes)
+            messages = await channel.history(limit=1).flatten()  # Récupérer le dernier message du salon
+            if messages:
+                message = messages[0]
+                embed = discord.Embed(
+                    title=f"Nouveau salon créé : {channel.name}",
+                    description=f"Contenu initial : {message.content}",
+                    color=discord.Color.green()
+                )
+                payload = {
+                    "embeds": [embed.to_dict()]
+                }
+                requests.post(webhook_url, json=payload)
 
-    for tag in inside_article_tags:
-        h3_tags = tag.find_all("h3")
-        links = tag.find_all("a")
-
-        for h3 in h3_tags:
-            h3_text = h3.text.strip()
-            h3_link = None
-
-            for link in links:
-                link_text = link.get("href", "").strip()
-                if link_text.startswith("https://airdrops.io/"):
-                    h3_link = link_text
-                    break
-
-            h3_and_links.append((h3_text, h3_link))
-
-    return h3_and_links
-
-
-if __name__ == "__main__":
-    url = "https://airdrops.io"  # Remplacez par l'URL du site web souhaité
-    h3_and_links = get_h3_and_links(url)
-
-    if h3_and_links:
-        print("Balises h3 et liens dans la classe 'inside-article' :")
-        now = datetime.now()
-        one_week_ago = now - timedelta(days=7)
-        for h3, link in h3_and_links:
-            if link is not None:
-                try:
-                    link_date = datetime.strptime(link.split("/")[-1], "%Y-%m-%d")
-                    if link_date >= one_week_ago:
-                        print(Fore.GREEN + "Name :", h3, "/ Lien :", link + Style.RESET_ALL)  # Vert pour les nouvelles balises h3
-                    else:
-                        print(Fore.YELLOW + "Name :", h3, "/ Lien :", link + Style.RESET_ALL)  # Orange pour les balises h3 anciennes
-                except ValueError:
-                    print("Name :", h3, "/ Lien :", link)
-            else:
-                print("Name :", h3, "/ Lien : None")
-    else:
-        print("Aucune balise h3 ou lien trouvés dans la classe 'inside-article' sur la page.")
+client = MyClient()
+client.run()  # Pas besoin de spécifier le jeton (token) du bot

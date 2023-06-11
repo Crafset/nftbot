81de1976c3c118f32bd0c51a6daae7b9ecefefe9
Crafset <82468619+Crafset@users.noreply.github.com>
2023-06-11 02:28:25 +0200

    Update nftbot.py

--- nftbot.py
+++ nftbot.py
@@ -1,39 +1,61 @@
 import requests
 
 def get_top_cryptos():
-    url = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=50&sortBy=market_cap&sortType=asc&convert=USD&cryptoType=all&tagType=all"
-    response = requests.get(url)
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+    parameters = {
+        "start": "1",
+        "limit": "50",
+        "sort": "market_cap",
+        "convert": "USD",
+        "aux": "num_market_pairs,tags"
+    }
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": "VOTRE_CLE_API"
+    }
+    
+    response = requests.get(url, params=parameters, headers=headers)
     data = response.json()
 
-    if response.status_code == 200:
-        return data["data"]["cryptoCurrencyList"]
+    if "data" in data:
+        return data["data"]
+    else:
+        return None
 
-    return None
+def filter_cryptos(cryptos):
+    filtered_cryptos = []
+    
+    for crypto in cryptos:
+        if crypto["quote"]["USD"]["market_cap"] < 100000000:
+            if len(crypto["platform"]) > 0:
+                filtered_cryptos.append(crypto)
+    
+    return filtered_cryptos
 
-def get_wallets(crypto_id):
-    url = f"https://api.coinmarketcap.com/data-api/v3/cryptocurrency/wallets/rankings/latest?id={crypto_id}&start=1&limit=100&score=market_cap"
-    response = requests.get(url)
-    data = response.json()
+def get_cryptos_with_large_wallets(cryptos):
+    cryptos_with_large_wallets = []
+    
+    for crypto in cryptos:
+        if crypto["tags"] and "wallet" in crypto["tags"]:
+            if crypto["num_market_pairs"] > 1:
+                cryptos_with_large_wallets.append(crypto)
+    
+    return cryptos_with_large_wallets
 
-    if response.status_code == 200:
-        return data["data"]["cryptoCurrency"]["wallets"]
+# Récupération des cryptomonnaies du top 50
+cryptos = get_top_cryptos()
 
-    return None
+if cryptos:
+    # Filtrage des cryptomonnaies selon les critères spécifiés
+    filtered_cryptos = filter_cryptos(cryptos)
 
-def print_wallets():
-    top_cryptos = get_top_cryptos()
+    # Récupération des cryptomonnaies avec plusieurs gros wallets associés
+    cryptos_with_large_wallets = get_cryptos_with_large_wallets(filtered_cryptos)
 
-    if top_cryptos is not None:
-        for crypto in top_cryptos:
-            crypto_id = crypto["id"]
-            wallets = get_wallets(crypto_id)
-            if wallets is not None:
-                quote = crypto["quote"]["USD"]
-                market_cap = quote["price"] * crypto["circulatingSupply"]
-                if market_cap < 100_000_000:
-                    print(f"--- {crypto['name']} Wallets ---")
-                    for wallet in wallets:
-                        print(wallet["address"])
-                    print()
-
-print_wallets()
+    # Affichage des résultats
+    for crypto in cryptos_with_large_wallets:
+        print("Nom : ", crypto["name"])
+        print("Wallet : ", crypto["platform"]["token_address"])
+        print("--------------------------------------")
+else:
+    print("Erreur lors de la récupération des données.")

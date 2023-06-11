03dff53dce8e5979213df71b3dc514167eed65b7
Crafset <82468619+Crafset@users.noreply.github.com>
2023-06-11 02:18:29 +0200

    Update nftbot.py

--- nftbot.py
+++ nftbot.py
@@ -1,7 +1,6 @@
 import requests
 
 def get_top_cryptos():
-    # Appel à l'API CoinMarketCap pour récupérer les informations sur le top 50 des cryptomonnaies
     url = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=50&sortBy=market_cap&sortType=asc&convert=USD&cryptoType=all&tagType=all"
     response = requests.get(url)
     data = response.json()
@@ -12,7 +11,6 @@ def get_top_cryptos():
     return None
 
 def get_wallets(crypto_id):
-    # Appel à l'API CoinMarketCap pour récupérer les informations sur les portefeuilles d'une cryptomonnaie
     url = f"https://api.coinmarketcap.com/data-api/v3/cryptocurrency/wallets/rankings/latest?id={crypto_id}&start=1&limit=100&score=market_cap"
     response = requests.get(url)
     data = response.json()
@@ -27,10 +25,12 @@ def print_wallets():
 
     if top_cryptos is not None:
         for crypto in top_cryptos:
-            if crypto["marketCap"] < 100_000_000:
-                crypto_id = crypto["id"]
-                wallets = get_wallets(crypto_id)
-                if wallets is not None:
+            crypto_id = crypto["id"]
+            wallets = get_wallets(crypto_id)
+            if wallets is not None:
+                quote = crypto["quote"]["USD"]
+                market_cap = quote["price"] * crypto["circulatingSupply"]
+                if market_cap < 100_000_000:
                     print(f"--- {crypto['name']} Wallets ---")
                     for wallet in wallets:
                         print(wallet["address"])
